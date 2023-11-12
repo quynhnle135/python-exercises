cde1b5bc5b23075cd01ad6076533383cf7cd42eb
Quinn Le <quynhnle135@gmail.com>
2023-11-12 09:59:15 -0500

    Init and did exercise 1

---
+++ exercise_1.py
@@ -0,0 +1,12 @@
+def greeting():
+    print("Hello, World!")
+    name = input("What is your name?\n")
+    print(f"Hello, {name}")
+
+
+def main():
+    greeting()
+
+
+if __name__ == "__main__":
+    main()
